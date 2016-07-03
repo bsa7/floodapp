c639f7d178ad6253537b409e2188e2cf15ced316
Sergeij Belevskij <bbelevskij@gmail.com>
2016-07-03 19:10:48 +0500

    Replace instance variable to local var (@phrases)

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,12 +1,12 @@
 # serve random sentence queries
 class SentencesController < ApplicationController
   def sampler
-    @phrases = {
+    phrases = {
       sentence: Sentence.find_phrases_by(text: params[:text]),
       random_icon: Icon.sample_icon
     }
     respond_to do |format|
-      format.json { render json: @phrases }
+      format.json { render json: phrases }
       format.html { render :sampler }
     end
   end
